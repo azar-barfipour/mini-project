a54d43df7043c4ffe810a1cb3964bae17f2e779e
Azar <azar.barfipour@gmail.com>
2021-12-05 15:30:11 -0800

    add gpd for Iran-Canada

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // for Japan
 const OKU = 100000000;
 const TYO = 1000000000000;
+const BILLION = 1000000000;
 const japanNumToCanadaDoller = (oku_yen, unit) => {
-  return (oku_yen * unit);
+  return oku_yen * unit;
 };
 
 // setup
@@ -19,7 +20,36 @@ const dataForJapanGDP = {
         japanNumToCanadaDoller(1.105, TYO),
         japanNumToCanadaDoller(3.133, TYO),
         japanNumToCanadaDoller(4.888, TYO),
-        japanNumToCanadaDoller(5.700, TYO),
+        japanNumToCanadaDoller(5.7, TYO),
+      ],
+    },
+    {
+      label: "Canada GDP (USD)",
+      backgroundColor: "rgb(0, 99, 132)",
+      borderColor: "rgb(0, 99, 132)",
+      data: [
+        japanNumToCanadaDoller(992.7, OKU),
+        japanNumToCanadaDoller(2739, OKU),
+        japanNumToCanadaDoller(5939, OKU),
+        japanNumToCanadaDoller(7448, OKU),
+        japanNumToCanadaDoller(1.167, TYO),
+      ],
+    },
+  ],
+};
+const dataForIranGDP = {
+  labels: labelsForJapanGDP,
+  datasets: [
+    {
+      label: "Iran GDP (USD)",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgb(255, 99, 132)",
+      data: [
+        japanNumToCanadaDoller(10.98, BILLION),
+        japanNumToCanadaDoller(94.36, BILLION),
+        japanNumToCanadaDoller(128.8, BILLION),
+        japanNumToCanadaDoller(109.6, BILLION),
+        japanNumToCanadaDoller(486.8, BILLION),
       ],
     },
     {
@@ -43,6 +73,15 @@ const config = {
   data: dataForJapanGDP,
   options: {},
 };
+const configIran = {
+  type: "line",
+  data: dataForIranGDP,
+  options: {},
+};
 
 // create chart
 const myChart = new Chart(document.getElementById("jp-canada-gdp"), config);
+const IranChart = new Chart(
+  document.getElementById("Canada-Iran-gdp"),
+  configIran
+);
